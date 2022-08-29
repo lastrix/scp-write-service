20853211534e531dbc749c5df304126e92996064
lastrix <2@lastrix.org>
2022-08-29 09:23:07 +0300

    initial

---
+++ scp-write-service-app/src/main/java/com/lastrix/scp/writeservice/ScpWriteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lastrix.scp.writeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScpWriteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScpWriteServiceApplication.class, args);
+	}
+
+}
---
+++ scp-write-service-app/src/test/java/com/lastrix/scp/writeservice/ScpWriteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastrix.scp.writeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScpWriteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
