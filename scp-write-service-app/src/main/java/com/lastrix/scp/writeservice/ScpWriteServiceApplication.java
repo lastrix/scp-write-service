4bd367328670e16509740b1342e5e0f47a1a101f
lastrix <2@lastrix.org>
2022-09-02 10:33:26 +0300

    ! move schema init into db lib (all db-related tools will go there too)

--- scp-write-service-app/src/main/java/com/lastrix/scp/writeservice/ScpWriteServiceApplication.java
+++ scp-write-service-app/src/main/java/com/lastrix/scp/writeservice/ScpWriteServiceApplication.java
@@ -1,9 +1,12 @@
 package com.lastrix.scp.writeservice;
 
+import com.lastrix.scp.lib.db.SchemaInit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "com.lastrix.scp.writeservice")
+@Import(SchemaInit.class)
 public class ScpWriteServiceApplication {
 
     public static void main(String[] args) {
